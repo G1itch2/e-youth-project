0f55bc03778eb64c554635a980b6b7b00cc28bee
Glitch <155385126+G1itch2@users.noreply.github.com>
2026-07-04 19:14:14 +0300

    Update cartRoutes.js

---
+++ routes/cartRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const { 
+  getCart, 
+  addToCart, 
+  updateCartItem, 
+  removeFromCart 
+} = require('../controllers/cartController');
+
+router.route('/')
+  .get(getCart)
+  .post(addToCart);
+
+router.route('/:productId')
+  .put(updateCartItem)
+  .delete(removeFromCart);
+
+module.exports = router;
